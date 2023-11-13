f50877a212235fd5e97c2a58369fb088554aee56
gbalaji <gadi.balaji@epsilon.com>
2023-11-13 20:21:36 +0530

    adding frontend.sh file to expense-shell repo

--- frontend.sh
+++ frontend.sh
@@ -1,14 +1,19 @@
+echo -e "\e31m installing nginx package\e[0m"
 dnf install nginx -y
 
+echo -e "\e32m clearing nginx file content \e[0m"
 rm -rf /usr/share/nginx/html/*
 
+echo -e "\e33m copying nginx reverse proxy configuration file \e[0m"
 cp expense.conf /etc/nginx/default.d/expense.conf
 
+echo -e "\e34m downloading application content \e[0m"
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
 
+echo -e "\e35m extracting application code content \e[0m"
 cd /usr/share/nginx/html
-
 unzip /tmp/frontend.zip
 
+echo -e "\e36m enabling and starting nginx service\e[0m"
 systemctl enable nginx
 systemctl restart nginx
